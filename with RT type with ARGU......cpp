ea39efe850c6a1e284b53c3dde89739866e88251
Abhay Surwade <abhaysurwade79@gmail.com>
2025-06-21 14:02:59 +0530

    Add files via upload

---
+++ with RT type with ARGU......cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+float sum();
+float sub();
+float multi();
+float divide();
+main()
+{
+	printf("%f\n",sum());
+	printf("%f\n",sub());
+	printf("%f\n",multi());
+	printf("%f\n",divide());
+	
+}
+float sum()
+{
+int a;
+float b;
+printf("enter the num a&b");
+scanf("%d%f",&a,&b);
+return a+b;
+}
+float sub()
+{
+int a;
+float b;
+printf("enter the num a&b");
+scanf("%d%f",&a,&b);
+return a-b;
+}
+float multi()
+{
+int a;
+float b;
+printf("enter the num a&b");
+scanf("%d%f",&a,&b);
+return a*b;
+}
+float divide()
+{
+int a;
+float b;
+printf("enter the num a&b");
+scanf("%d%f",&a,&b);
+return a/b;
+}
