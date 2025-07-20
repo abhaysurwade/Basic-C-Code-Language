9864cd89d1b1f7aeb0afd35a1a00d9a46438659f
Abhay Surwade <abhaysurwade79@gmail.com>
2025-07-20 12:09:33 +0530

    Add files via upload

---
+++ ascending order by array.cpp
@@ -0,0 +1,29 @@
+// ascending order by array..
+
+#include<stdio.h>
+main()
+{
+	int a[5],i,j,temp;
+	for(i=0;i<5;i++)
+	{
+	   printf("enter the number");
+	   scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+       for(j=i+1;j<5;j++)
+       {
+       if(a[i]>a[j])		
+       {
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+       }
+    }
+}
+for(i=0;i<5;i++)
+{
+    printf("%d",a[i]);
+}
+}
+
